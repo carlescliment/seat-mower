16265fdc7ede918a0dadcfbd134eaa0a11b8098e
Carles Climent <carlescliment@gmail.com>
2022-04-23 13:50:32 +0200

    Refactoring: Extract Compass class to deal with facings

--- src/mower.py
+++ src/mower.py
@@ -1,4 +1,4 @@
-class Mower:
+class Compass:
     NORTH = 'N'
     WEST = 'W'
     SOUTH = 'S'
@@ -7,17 +7,11 @@ class Mower:
     LEFT = 'L'
     RIGHT = 'R'
 
-    def __init__(self, coordinate_x: int, coordinate_y: int, facing: str):
+    def __init__(self, facing: str):
         self.__facing = facing
-        self.__coordinate_x = coordinate_x
-        self.__coordinate_y = coordinate_y
 
-    @classmethod
-    def deploy(cls, coordinate_x: int, coordinate_y: int, facing: str):
-        return cls(coordinate_x, coordinate_y, facing)
-
-    def execute(self, commands: str) -> 'Mower':
-        facings_when_spinning = {
+    def turn(self, side: str) -> str:
+        facings_when_turning = {
             self.NORTH: {
                 self.LEFT: self.WEST,
                 self.RIGHT: self.EAST,
@@ -35,7 +29,23 @@ class Mower:
                 self.RIGHT: self.SOUTH,
             },
         }
-        self.__facing = facings_when_spinning[self.__facing][commands]
+
+        return facings_when_turning[self.__facing][side]
+
+
+class Mower:
+
+    def __init__(self, coordinate_x: int, coordinate_y: int, facing: str):
+        self.__facing = facing
+        self.__coordinate_x = coordinate_x
+        self.__coordinate_y = coordinate_y
+
+    @classmethod
+    def deploy(cls, coordinate_x: int, coordinate_y: int, facing: str):
+        return cls(coordinate_x, coordinate_y, facing)
+
+    def execute(self, commands: str) -> 'Mower':
+        self.__facing = Compass(self.__facing).turn(commands)
 
         return self
 
