81722d846fe1c47da3e4095c460b689d148f8b55
Carles Climent <carlescliment@gmail.com>
2022-04-23 14:13:36 +0200

    Refactoring: Replace conditional with strategy. Rename Compass to Navigator

--- src/mower.py
+++ src/mower.py
@@ -1,58 +1,100 @@
-class Compass:
-    NORTH = 'N'
-    WEST = 'W'
-    SOUTH = 'S'
-    EAST = 'E'
-
+class Turn:
     LEFT = 'L'
     RIGHT = 'R'
 
-    def __init__(self, cardinal_point: str):
-        self.__cardinal_point = cardinal_point
 
-    def where_is_it_pointing_to(self):
-        return self.__cardinal_point
+class CardinalPoint:
 
-    def turn(self, side: str) -> 'Compass':
-        cardinal_point_when_turning = {
-            self.NORTH: {
-                self.LEFT: self.WEST,
-                self.RIGHT: self.EAST,
-            },
-            self.WEST: {
-                self.LEFT: self.SOUTH,
-                self.RIGHT: self.NORTH,
-            },
-            self.SOUTH: {
-                self.LEFT: self.EAST,
-                self.RIGHT: self.WEST,
-            },
-            self.EAST: {
-                self.LEFT: self.NORTH,
-                self.RIGHT: self.SOUTH,
-            },
+    def if_turning(self, side):
+        raise NotImplementedError()
+
+    def __repr__(self):
+        raise NotImplementedError()
+
+    @staticmethod
+    def create(cardinal_point: str):
+        cardinal_points = {
+            'N': North(),
+            'S': South(),
+            'E': East(),
+            'W': West()
         }
 
-        self.__cardinal_point = cardinal_point_when_turning[self.__cardinal_point][side]
+        return cardinal_points[cardinal_point]
+
+
+class North(CardinalPoint):
+    def if_turning(self, side: str):
+        if side == Turn.LEFT:
+            return West()
+
+        return East()
+
+    def __repr__(self):
+        return 'N'
+
+
+class West(CardinalPoint):
+    def if_turning(self, side):
+        if side == Turn.LEFT:
+            return South()
+
+        return North()
+
+    def __repr__(self):
+        return 'W'
+
+
+class South(CardinalPoint):
+    def if_turning(self, side):
+        if side == Turn.LEFT:
+            return East()
+
+        return West()
+
+    def __repr__(self):
+        return 'S'
+
+
+class East(CardinalPoint):
+    def if_turning(self, side):
+        if side == Turn.LEFT:
+            return North()
+
+        return South()
+
+    def __repr__(self):
+        return 'E'
+
+
+class Navigator:
+    def __init__(self, initial_cardinal_point: str):
+        self.__cardinal_point = CardinalPoint.create(initial_cardinal_point)
+
+    def where_is_it_currently_facing(self):
+        return str(self.__cardinal_point)
+
+    def turn(self, side: str) -> 'Navigator':
+        self.__cardinal_point = self.__cardinal_point.if_turning(side)
 
         return self
 
 
 class Mower:
 
-    def __init__(self, coordinate_x: int, coordinate_y: int, compass: Compass):
-        self.__compass = compass
+    def __init__(self, coordinate_x: int, coordinate_y: int, navigator: Navigator):
+        self.__navigator = navigator
         self.__coordinate_x = coordinate_x
         self.__coordinate_y = coordinate_y
 
     @classmethod
     def deploy(cls, coordinate_x: int, coordinate_y: int, facing: str):
-        return cls(coordinate_x, coordinate_y, Compass(facing))
+        return cls(coordinate_x, coordinate_y, Navigator(facing))
 
     def execute(self, commands: str) -> 'Mower':
-        self.__compass.turn(commands)
+        self.__navigator.turn(commands)
 
         return self
 
     def report(self) -> str:
-        return f'{self.__coordinate_x} {self.__coordinate_y} {self.__compass.where_is_it_pointing_to()}'
+        return f'{self.__coordinate_x} {self.__coordinate_y} {self.__navigator.where_is_it_currently_facing()}'
