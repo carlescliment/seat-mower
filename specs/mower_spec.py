f6be80e4a3d180b17457348929d38e1ddf866861
Carles Climent <carlescliment@gmail.com>
2022-04-23 13:03:02 +0200

    Deploy the mower on a given X coordinate

--- specs/mower_spec.py
+++ specs/mower_spec.py
@@ -18,21 +18,26 @@ SOUTH = 'S'
 with description('Seat Mower'):
     with context('when deploying the mower'):
         with it('can be faced north'):
-            mower = Mower.deploy(NORTH)
+            mower = Mower.deploy(0, NORTH)
 
-            expect(mower.report()).to(equal(NORTH))
+            expect(mower.report()).to(equal(f'0 {NORTH}'))
 
         with it('can be faced east'):
-            mower = Mower.deploy(EAST)
+            mower = Mower.deploy(0, EAST)
 
-            expect(mower.report()).to(equal(EAST))
+            expect(mower.report()).to(equal(f'0 {EAST}'))
 
         with it('can be faced west'):
-            mower = Mower.deploy(WEST)
+            mower = Mower.deploy(0, WEST)
 
-            expect(mower.report()).to(equal(WEST))
+            expect(mower.report()).to(equal(f'0 {WEST}'))
 
         with it('can be faced south'):
-            mower = Mower.deploy(SOUTH)
+            mower = Mower.deploy(0, SOUTH)
 
-            expect(mower.report()).to(equal(SOUTH))
+            expect(mower.report()).to(equal(f'0 {SOUTH}'))
+
+        with it('can be placed on a given X coordinate'):
+            mower = Mower.deploy(1, SOUTH)
+
+            expect(mower.report()).to(equal(f'1 {SOUTH}'))
--- src/mower.py
+++ src/mower.py
@@ -1,10 +1,11 @@
 class Mower:
-    def __init__(self, facing):
+    def __init__(self, coordinate_x, facing):
         self._facing = facing
+        self._coordinate_x = coordinate_x
 
     @classmethod
-    def deploy(cls, facing: str):
-        return cls(facing)
+    def deploy(cls, coordinate_x, facing: str):
+        return cls(coordinate_x, facing)
 
     def report(self) -> str:
-        return self._facing
+        return f'{self._coordinate_x} {self._facing}'
